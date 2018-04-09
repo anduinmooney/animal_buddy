f771e80e707b5edb812e81e4cb6c246afa50b979
Anduin Mooney <anduinmooney@gmail.com>
2018-04-09 15:10:46 -0700

    Add search activity

--- app/src/main/java/com/epicodus/animalbuddy/SearchActivity.java
+++ app/src/main/java/com/epicodus/animalbuddy/SearchActivity.java
@@ -1,6 +1,5 @@
 package com.epicodus.animalbuddy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,15 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class AboutActivity extends AppCompatActivity
+public class SearchActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -58,7 +55,7 @@ public class AboutActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.about, menu);
+        getMenuInflater().inflate(R.menu.search, menu);
         return true;
     }
 
@@ -83,18 +80,17 @@ public class AboutActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_about) {
-            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_search) {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_saved) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_email) {
+        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_shelter) {
+        } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_random) {
+        } else if (id == R.id.nav_send) {
 
         }
 
