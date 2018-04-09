87a54890e1b76c44904694029240966482b77e27
Anduin Mooney <anduinmooney@gmail.com>
2018-04-09 11:53:38 -0700

    Get Navbar intent to work

--- app/src/main/java/com/epicodus/animalbuddy/MainActivity.java
+++ app/src/main/java/com/epicodus/animalbuddy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.animalbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_about) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_saved) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_email) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_shelter) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_random) {
 
         }
 
