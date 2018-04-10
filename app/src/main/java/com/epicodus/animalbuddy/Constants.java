0f0fe3849da3425723534f8ff37230e54dae84a6
Anduin Mooney <anduinmooney@gmail.com>
2018-04-10 09:39:32 -0700

    Add dummy edittext and button for main search activity for API call testing

--- app/src/main/java/com/epicodus/animalbuddy/Constants.java
+++ app/src/main/java/com/epicodus/animalbuddy/Constants.java
@@ -6,6 +6,6 @@ package com.epicodus.animalbuddy;
 
 public class Constants {
     public static final String PET_TOKEN = BuildConfig.PET_TOKEN;
-    public static final String PET_BASE_URL = ("http://api.petfinder.com/pet.find?key=" + PET_TOKEN + "&animal=cat&location=97203&output=basic&format=json&callback=?");
+    public static final String PET_BASE_URL = ("http://api.petfinder.com/pet.find?key=" + PET_TOKEN + "&animal=cat&output=basic&format=json&callback=?");
     public static final String PET_LOCATION_QUERY_PARAMETER = "location";
 }
--- app/src/main/java/com/epicodus/animalbuddy/PetService.java
+++ app/src/main/java/com/epicodus/animalbuddy/PetService.java
@@ -1,6 +1,12 @@
 package com.epicodus.animalbuddy;
 
-import javax.security.auth.callback.Callback;
+
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 /**
  * Created by Guest on 4/10/18.
@@ -10,5 +16,22 @@ public class PetService {
 
     public static void findPets(String location, Callback callback) {
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.PET_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.PET_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.PET_TOKEN)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
+
 }
+
+
