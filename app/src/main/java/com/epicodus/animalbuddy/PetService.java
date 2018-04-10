9942e1c015a43286f8f5cd79db165bc33b158dce
Anduin Mooney <anduinmooney@gmail.com>
2018-04-10 09:29:45 -0700

    Create PetService class

---
+++ app/src/main/java/com/epicodus/animalbuddy/PetService.java
@@ -0,0 +1,14 @@
+package com.epicodus.animalbuddy;
+
+import javax.security.auth.callback.Callback;
+
+/**
+ * Created by Guest on 4/10/18.
+ */
+
+public class PetService {
+
+    public static void findPets(String location, Callback callback) {
+
+    }
+}
