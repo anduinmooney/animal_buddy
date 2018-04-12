da4f94363f9078f2d407e874a135cface32376ea
Student <example@example.com>
2018-04-12 12:31:41 -0700

    Add call to shelters to ShelterService

--- app/src/main/java/com/epicodus/animalbuddy/services/ShelterService.java
+++ app/src/main/java/com/epicodus/animalbuddy/services/ShelterService.java
@@ -1,17 +1,22 @@
 package com.epicodus.animalbuddy.services;
 
 import com.epicodus.animalbuddy.Constants;
+import com.epicodus.animalbuddy.models.Shelter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
-
-/**
- * Created by Guest on 4/12/18.
- */
 
 public class ShelterService {
 
@@ -32,6 +37,37 @@ public class ShelterService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    public ArrayList<Shelter> processResults (Response response) {
+        ArrayList<Shelter> shelterList = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+
+            JSONObject shelterJSON = new JSONObject(jsonData);
+            JSONArray shelterListJSON = shelterJSON.getJSONObject("petfinder").getJSONObject("shelters").getJSONArray("shelter");
+            for (int i = 0; i < shelterListJSON.length(); i++) {
+                JSONObject shelterObjectJSON = shelterListJSON.getJSONObject(i);
+                String name = shelterObjectJSON.getJSONObject("name").getString("$t");
+                String phone = shelterObjectJSON.getJSONObject("phone").getString("$t");
+                String state = shelterObjectJSON.getJSONObject("state").getString("$t");
+                String email = shelterObjectJSON.getJSONObject("email").getString("$t");
+                String city = shelterObjectJSON.getJSONObject("city").getString("$t");
+                String zip = shelterObjectJSON.getJSONObject("zip").getString("$t");
+
+                Shelter shelter = new Shelter(name, phone, state, email, city, zip);
+                shelterList.add(shelter);
+            }
+        }
+
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+        return shelterList;
     }
 
 }
